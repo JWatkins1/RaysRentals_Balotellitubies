0311636721046f3c691afb7dbfad5efc2673a92a
JakeWatkins <jake.watkins@myfuturesbright.com>
2015-10-20 14:43:31 +0100

    All Customer classes finito

---
+++ src/main/java/uk/co/brightfuture/RaysRentals_Balotellitubies/Model/Customer.java
@@ -0,0 +1,6 @@
+package uk.co.brightfuture.RaysRentals_Balotellitubies.Model;
+
+public interface Customer {
+
+	public void saveCustomer(CustomerModel customerModel);
+}
---
+++ src/main/java/uk/co/brightfuture/RaysRentals_Balotellitubies/Model/CustomerModel.java
@@ -0,0 +1,116 @@
+package uk.co.brightfuture.RaysRentals_Balotellitubies.Model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+import org.joda.time.LocalDateTime;
+
+@Entity
+@Table(name = "Customer")
+public class CustomerModel implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6208866716064053651L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "date_joined")
+	@Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDateTime")
+	private LocalDateTime joinDate;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "post_code")
+	private String postCode;
+	
+	@Column(name = "phone_number")
+	private String phoneNumber;
+	
+	@Column(name = "number_of_rented_bikes")
+	private int rentedBikes;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public LocalDateTime getJoinDate() {
+		return joinDate;
+	}
+	
+	public void setJoinDate(LocalDateTime joinDate) {
+		this.joinDate = joinDate;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getPostCode() {
+		return postCode;
+	}
+	
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public int getRentedBikes() {
+		return rentedBikes;
+	}
+	
+	public void setRentedBikes(int numberOfRentedBikes) {
+		this.rentedBikes = numberOfRentedBikes;
+	}
+}
