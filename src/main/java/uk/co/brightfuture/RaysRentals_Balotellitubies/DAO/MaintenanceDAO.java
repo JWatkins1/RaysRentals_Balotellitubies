0601cbee0a1c5a1468c98f2f4fc90695fceb6523
JakeWatkins <jake.watkins@myfuturesbright.com>
2015-10-26 13:19:35 +0000

    Updated Maintenance class files

--- src/main/java/uk/co/brightfuture/RaysRentals_Balotellitubies/DAO/MaintenanceDAO.java
+++ src/main/java/uk/co/brightfuture/RaysRentals_Balotellitubies/DAO/MaintenanceDAO.java
@@ -1,5 +1,8 @@
 package uk.co.brightfuture.RaysRentals_Balotellitubies.DAO;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,17 +10,25 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 import uk.co.brightfuture.RaysRentals_Balotellitubies.Model.MaintenanceModel;
 
-public class MaintenanceDAO implements Maintenance{
-	
-	
+public class MaintenanceDAO implements Maintenance {
+
 	@Autowired
 	@Qualifier("sessionFactory")
 	public SessionFactory sessionFactory;
-	
+
 	public void maintenance(MaintenanceModel maintenanceModel) {
-		
+
 		Session session = sessionFactory.openSession();
 		session.save(maintenanceModel);
+	}
+	
+	@Override
+	public List<MaintenanceModel> retrieveAllMaintenanceReports() {
 		
+		Session session = sessionFactory.openSession();
+		Criteria criteria = session.createCriteria(MaintenanceModel.class);
+		List<MaintenanceModel> maintenance = criteria.list();
+		
+		return maintenance;
 	}
 }
--- src/main/java/uk/co/brightfuture/RaysRentals_Balotellitubies/Services/MaintenanceService.java
+++ src/main/java/uk/co/brightfuture/RaysRentals_Balotellitubies/Services/MaintenanceService.java
@@ -1,8 +1,13 @@
 package uk.co.brightfuture.RaysRentals_Balotellitubies.Services;
 
+import java.util.Date;
+import java.util.List;
+
+import uk.co.brightfuture.RaysRentals_Balotellitubies.Model.MaintenanceModel;
+
 public interface MaintenanceService {
-	
-	
-	public void maintenanceRecord();
-	
+
+	public void recordMaintenance(String damageDescription, int costOfRepair, Date repairDate, String repairSolution);
+
+	public List<MaintenanceModel> retrieveAllMaintenanceReports();
 }
